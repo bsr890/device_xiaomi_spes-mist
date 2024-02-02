cb593038bb7a09b749a88595d1703b8a07fc7d14
PugzAreCute <me@pugzarecute.com>
2024-02-02 16:46:11 +0530

    Revert "spes: Support doze double/single tap sensor"
    This reverts commit 95670ad7d486a6a4e867f4a1e0af7f4de39b651b.

--- device.mk
+++ device.mk
@@ -125,9 +125,6 @@ PRODUCT_PACKAGES += \
     init.device.rc
 
 # Sensors
-PRODUCT_PACKAGES += \
-    sensors.xiaomi
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
